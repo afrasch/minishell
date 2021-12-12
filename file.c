3419a32de7e7368ad6cd4b6ae627076686ad5fef
Alisja Frasch <afrasch@1-B-4.42heilbronn.de>
2021-12-12 15:32:30 +0100

    git stuff

--- file.c
+++ file.c
@@ -2,3 +2,10 @@ hello back Elenz
 
 Here comes the merge
 Here come the merge jo jo
+
+git branch allie // erstellt branch "allie"
+git checkout allie // geht in "allie branch"
+git push --set-upstream origin allie // activates "push" in branch allie
+git branch // shows all branches and where I am
+git pull origin main // pulls from branch main
+git fetch // get git branch changes
