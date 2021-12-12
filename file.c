d0b3823c4ae31b23451473e63a0d5e011aa1073a
Alisja Frasch <afrasch@1-B-4.42heilbronn.de>
2021-12-12 18:07:33 +0100

    file.c changes

--- file.c
+++ file.c
@@ -2,7 +2,10 @@ git branch allie // erstellt branch "allie"
 git checkout allie // geht in "allie" branch
 git push --set-upstream origin allie // activates "push" in branch allie
 git branch // shows all branches and where I am
-git pull origin main // pulls from branch main
+git pull origin main // pulls from branch main into where I am now
 git fetch // get git branch changes
 
+git add .
+git commit -m "<changes>"
+git push
 
