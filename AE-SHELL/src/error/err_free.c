8edaeb5943695dc120be996eb77b15a66ba0227b
Alisja Frasch <afrasch@1-D-4.42heilbronn.de>
2022-01-10 17:55:28 +0100

    17:55

--- AE-SHELL/src/error/err_free.c
+++ AE-SHELL/src/error/err_free.c
@@ -1,11 +1,11 @@
-#include "../includes/minishell.h"
+#include "minishell.h"
 
-void	reset_frame(t_frame *frame)
-{
-	if (frame != NULL)
-	{
-		if (frame->cn != NULL)
-			ft_clear_nodes(&frame->cn);
-		/* Hier muss dann viel gefreet werden, bei leaks hier gucken */
-	}
-}
+// void	reset_frame(t_frame *frame)
+// {
+// 	if (frame != NULL)
+// 	{
+// 		if (frame->cn != NULL)
+// 			ft_clear_nodes(&frame->cn);
+// 		/* Hier muss dann viel gefreet werden, bei leaks hier gucken */
+// 	}
+// }
