137805f9d49c200b7aa48ea82b1b824ca706fba5
Alisja Frasch <afrasch@1-B-8.42heilbronn.de>
2022-02-15 17:09:11 +0100

    merge

--- AE-SHELL/src/error/check_invalid_input_2.c
+++ AE-SHELL/src/error/check_invalid_input_2.c
@@ -4,15 +4,16 @@ static int	check_slashes(t_node *node, t_frame* frame)
 {
 	if (ft_strncmp(node->content, "/", 1) == 0)
 	{
-		if (access(node->content, F_OK) < 0)//geht das ?
+		if (ft_strlen(node->content) == 1)
+		{frame->e_status = 126;
+			return (print_error(-2, node->content, NULL, "is a directory"));}
+			// return (print_error(SHELLNAME, node->content, NULL, "is a directory"));
+		else if (access(node->content, F_OK) < 0)
 		{
 			frame->e_status = 127;
-			return (print_error(SHELLNAME, node->content, NULL, NULL));//nsfod
-			// return (print_error(errno, node->content, NULL, NULL));//nsfod
+			// return (print_error(SHELLNAME, node->content, NULL, NULL));//nsfod
+			return (print_error(errno, node->content, NULL, NULL));//nsfod
 		}
-		else if (!node->next->content)//TODO seg fault bei unset $PATH
-			return (print_error(SHELLNAME, node->content, NULL, "is a directory"));
-			// return (print_error(-2, node->content, NULL, "is a directory"));
 	}
 	return (0);
 }
@@ -27,16 +28,16 @@ static int	check_redir(t_node *node)
 			if ((node->type == D_REDIR_R && node->next->type != WORD)
 				|| (node->type == D_REDIR_L && node->next->type == D_REDIR_L)
 				|| (node->type == D_REDIR_L && node->next->type == S_REDIR_R))
-				return (print_error(SHELLNAME, node->next->content, NULL, "syntax error near unexpected token"));
-				// return (print_error(errno, node->next->content, NULL, "syntax error near unexpected token"));
+				// return (print_error(SHELLNAME, node->next->content, NULL, "syntax error near unexpected token"));
+				return (print_error(errno, node->next->content, NULL, "syntax error near unexpected token"));
 			if (node->type != D_REDIR_L && node->next->type == WORD
 				&& access(node->next->content, F_OK) == ERROR && node->type != S_REDIR_R)
-				return (print_error(SHELLNAME, node->next->content, NULL, "No such file or directory"));
-				// return (print_error(errno, node->next->content, NULL, "No such file or directory"));
+				// return (print_error(SHELLNAME, node->next->content, NULL, "No such file or directory"));
+				return (print_error(errno, node->next->content, NULL, "No such file or directory"));
 		}
 		else
-			return (print_error(SHELLNAME, "newline", NULL, "syntax error near unexpected token"));
-			// return (print_error(errno, "newline", NULL, "syntax error near unexpected token"));
+			// return (print_error(SHELLNAME, "newline", NULL, "syntax error near unexpected token"));
+			return (print_error(errno, "newline", NULL, "syntax error near unexpected token"));
 	}
 	return (0);
 }
@@ -44,11 +45,11 @@ static int	check_redir(t_node *node)
 static int	check_end_quotes(t_node *node)
 {
 	if (node->quote_st == SINGLE_Q)
-		return (print_error(SHELLNAME, "\'", NULL, "syntax error near single quotes"));
-		// return (print_error(-2, "\'", NULL, "syntax error near single quotes"));
+		// return (print_error(SHELLNAME, "\'", NULL, "syntax error near single quotes"));
+		return (print_error(-2, "\'", NULL, "syntax error near single quotes"));
 	if (node->quote_st == DOUBLE_Q)
-		return (print_error(SHELLNAME, "\"", NULL, "syntax error near double quotes"));
-		// return (print_error(-2, "\"", NULL, "syntax error near double quotes"));
+		// return (print_error(SHELLNAME, "\"", NULL, "syntax error near double quotes"));
+		return (print_error(-2, "\"", NULL, "syntax error near double quotes"));
 	return (0);
 }
 
--- AE-SHELL/src/error/error.c
+++ AE-SHELL/src/error/error.c
@@ -31,68 +31,68 @@ int	print_error_exit(char *cmd, char *arg, char *message)
 	return (ERROR);
 }
 
-// int	print_error(int err_no, char *cmd, char *arg, char *message)//TODO gegen NULL strings absichern
-// {
-// 	char *msg;
+int	print_error(int err_no, char *cmd, char *arg, char *message)//TODO gegen NULL strings absichern
+{
+	char *msg;
 
-// 	msg = strerror(err_no);
-// 	if (message == NULL && cmd != NULL && msg && !arg)
-// 		printf("%s: %s: %s\n", SHELLNAME, cmd, msg);
-// 	if (message == NULL && cmd != NULL && msg && arg)
-// 		printf("%s: %s: %s: %s\n", SHELLNAME, cmd, arg, msg);
-// 	else if (message == NULL && cmd == NULL && msg)
-// 		printf("%s: %s\n", SHELLNAME, msg);
-// 	else if (message && cmd && !arg)
+	msg = strerror(err_no);
+	if (message == NULL && cmd != NULL && msg && !arg)
+		printf("%s: %s: %s\n", SHELLNAME, cmd, msg);
+	if (message == NULL && cmd != NULL && msg && arg)
+		printf("%s: %s: %s: %s\n", SHELLNAME, cmd, arg, msg);
+	else if (message == NULL && cmd == NULL && msg)
+		printf("%s: %s\n", SHELLNAME, msg);
+	else if (message && cmd && !arg)
+	{
+		if (msg && ft_strcmp(msg, message) == 0)
+			printf("%s: %s: %s\n", SHELLNAME, cmd, msg);
+		else if (ft_strcmp(message, "syntax error near unexpected token") == 0)
+			printf("%s: %s `%s'\n", SHELLNAME, message, cmd);
+		else if (ft_strcmp("syntax error near double quotes", message) == 0)
+			printf("%s: %s: %s\n", SHELLNAME, message, cmd);
+		else if (ft_strcmp("syntax error near single quotes", message) == 0)
+			printf("%s: %s: %s\n", SHELLNAME, message, cmd);
+		else if (ft_strcmp("is a directory", message) == 0)
+			printf("%s: %s: %s\n", SHELLNAME, cmd, message);
+	}
+	else if (arg && ft_strcmp(message, "not a valid identifier") == 0)
+		printf("%s: %s: `%s': %s\n", SHELLNAME, cmd, arg, message);
+	return (ERROR);
+}
+
+//tjensen-hepple lösung
+// int	print_error(char *s1, char *s2, char *s3, char *message)
+// {
+// 	if (s1)
+// 		ft_putstr_fd(s1, 2);
+// 	if (s2)
 // 	{
-// 		if (msg && ft_strcmp(msg, message) == 0)
-// 			printf("%s: %s: %s\n", SHELLNAME, cmd, msg);
-// 		else if (ft_strcmp(message, "syntax error near unexpected token") == 0)
-// 			printf("%s: %s `%s'\n", SHELLNAME, message, cmd);
-// 		else if (ft_strcmp("syntax error near double quotes", message) == 0)
-// 			printf("%s: %s: %s\n", SHELLNAME, message, cmd);
-// 		else if (ft_strcmp("syntax error near single quotes", message) == 0)
-// 			printf("%s: %s: %s\n", SHELLNAME, message, cmd);
-// 		else if (ft_strcmp("is a directory", message) == 0)
-// 			printf("%s: %s: %s\n", SHELLNAME, cmd, message);
+// 		if (s1)
+// 			ft_putstr_fd(": ", 2);
+// 		ft_putstr_fd(s2, 2);
 // 	}
-// 	else if (arg && ft_strcmp(message, "not a valid identifier") == 0)
-// 		printf("%s: %s: `%s': %s\n", SHELLNAME, cmd, arg, message);
+// 	if (s3)
+// 	{
+// 		if (s1 || s2)
+// 			ft_putstr_fd(": ", 2);
+// 		ft_putstr_fd(s3, 2);
+// 	}
+// 	if (message)
+// 	{
+// 		if (s1 || s2 || s3)
+// 			ft_putstr_fd(": ", 2);
+// 		ft_putstr_fd(message, 2);
+// 	}
+// 	ft_putchar_fd('\n', 2);
 // 	return (ERROR);
 // }
 
-//tjensen-hepple lösung
-int	print_error(char *s1, char *s2, char *s3, char *message)
-{
-	if (s1)
-		ft_putstr_fd(s1, 2);
-	if (s2)
-	{
-		if (s1)
-			ft_putstr_fd(": ", 2);
-		ft_putstr_fd(s2, 2);
-	}
-	if (s3)
-	{
-		if (s1 || s2)
-			ft_putstr_fd(": ", 2);
-		ft_putstr_fd(s3, 2);
-	}
-	if (message)
-	{
-		if (s1 || s2 || s3)
-			ft_putstr_fd(": ", 2);
-		ft_putstr_fd(message, 2);
-	}
-	ft_putchar_fd('\n', 2);
-	return (ERROR);
-}
-
-int	print_error_errno(char *s1, char *s2, char *s3)
-{
-	print_error(s1, s2, s3, strerror(errno));
-	errno = 0;
-	return (ERROR);
-}
+// int	print_error_errno(char *s1, char *s2, char *s3)
+// {
+// 	print_error(s1, s2, s3, strerror(errno));
+// 	errno = 0;
+// 	return (ERROR);
+// }
 
 // void	exec_scmd_free_exit(int status, char **argv, t_list *l_free)
 // {
